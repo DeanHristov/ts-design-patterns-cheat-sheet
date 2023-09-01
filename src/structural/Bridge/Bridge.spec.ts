40fa6a5c42ebb2aa362283a312830cd7e9e4bcf9
D. Hristov <dean.hristovv@gmail.com>
2023-09-01 15:37:39 +0300

    Fixed the test heading in the bridge design pattern

--- src/structural/Bridge/Bridge.spec.ts
+++ src/structural/Bridge/Bridge.spec.ts
@@ -5,7 +5,7 @@ import ItalianRestaurant from './ItalianRestaurant';
 import VeggiePizza from './VeggiePizza';
 
 describe('Structural -> Bridge pattern', () => {
-  it('Should be able to deliver a pepperoni pizza from an American restaurant ', () => {
+  it('Should be able to deliver a Pepperoni pizza from an American restaurant ', () => {
     const outputMessage = `Deliver a pizza (Pepperoni) from American restaurant`;
     const americanRestaurant: AmericanRestaurant = new AmericanRestaurant(
       new PepperoniPizza('Pepperoni'),
@@ -14,7 +14,7 @@ describe('Structural -> Bridge pattern', () => {
     expect(americanRestaurant.deliver()).toMatch(outputMessage);
   });
 
-  it('Should be able to deliver a pepperoni pizza from an American restaurant ', () => {
+  it('Should be able to deliver a Veggie pizza from an Italian restaurant ', () => {
     const outputMessage = 'Deliver a pizza (Veggie) from Italian restaurant';
     const italianRestaurant: ItalianRestaurant = new ItalianRestaurant(
       new VeggiePizza('Veggie'),
